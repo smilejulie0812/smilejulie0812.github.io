14b7667bbb16ee1a0289751cb7c9ceba40b03a9c
Lee Juhyun <81246537+smilejulie0812@users.noreply.github.com>
2021-03-24 21:37:00 +0900

    Add files via upload

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "About",
+        "excerpt":"안녕하세요. 주리입니다.  ","categories": ["About"],
+        "tags": ["Introduce"],
+        "url": "/about/about/",
+        "teaser": null
+      }]
